495b32288b239337426696cb56c63252dcf8ef9e
umalrozi <umalrozi@email.com>
2023-01-23 14:38:26 -0800

    commit

---
+++ 02-Avaribles/index.js
@@ -0,0 +1,33 @@
+// A varibles is a container for sroring data 
+// A varibles behaves as if it was the value that is contains
+
+// two steps:
+// 1. declaration (var, let, const)
+//2. assigments (= assigments opeartor)
+
+
+
+
+// this number data type 
+// step one 
+let age;
+console.log("age");
+
+// step two 
+let ages;
+ages=22; // numbers 
+console.log(ages);
+
+// string data type
+let firstname ="umal";// string 
+console.log("hello pea" + firstname);
+
+// booleans
+let student=true;
+console.log(student);
+
+
+// you use to change this html
+document.getElementById("p1").innerHTML="Hello "+ firstname;
+document.getElementById("p2").innerHTML="you are  "+ ages + " years old ";
+document.getElementById("p3").innerHTML="Enter "+ student;
