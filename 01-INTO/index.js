1f1423a5b9ae5511e03057b98f1737dece341383
umalrozi <umalrozi@email.com>
2023-01-23 14:09:12 -0800

    commit

---
+++ 01-INTO/index.js
@@ -0,0 +1,16 @@
+console.log("hello world ");
+console.log("I lIKE SOMALIAND ");
+
+
+
+// like massege oo kaleh weyeh 
+window.alert("I hate, some peaple");
+
+
+// this a comment 
+
+/*
+this is a 
+multiple comment
+
+*/ 
