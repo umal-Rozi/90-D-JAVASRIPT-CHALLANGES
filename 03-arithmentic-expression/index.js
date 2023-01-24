4f240d1eadcd9a8754421649b63187e4ff7546d0
umalrozi <umalrozi@email.com>
2023-01-24 13:32:34 -0800

    commit

---
+++ 03-arithmentic-expression/index.js
@@ -0,0 +1,25 @@
+/*artithetic experession is a combination of
+   operands (value, variables, etc..)
+   operatores (+ - % /)
+   that can be evuluated to a value
+   ex.y=x+5;
+*/ 
+
+let student =21;
+
+student=student +1;
+student=student -1;
+student=student *2;
+student=student /2;
+
+let extrastudents = student % 3;
+
+console.log(student );
+
+student+=3;
+console.log(student);
+
+
+let result =1 +2 *(3+4);
+console.log(result);
+
