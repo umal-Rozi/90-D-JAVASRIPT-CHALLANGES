6cf8c550ae351e356a1713becf1fb370390c0572
umalrozi <umalrozi@email.com>
2023-01-25 15:41:20 -0800

    commit

---
+++ 07-math/script.js
@@ -0,0 +1,22 @@
+Math //js maths
+
+let x= 2;
+let y=5;
+let z= 9;
+let maximum;
+let minumum;
+
+//x=Math.round(x); // waa so dubid 
+//x=Math.floor(x); // isla so dubdi un
+//x=Math.ceil(x); // waxay u duubaysaa kor 
+//x=Math.pow(x,2);  // waa power
+//x=Math.sqrt(x); // squar weyeh
+//x=Math.abs(x); // waxy galinysa ama ka saaraysaa - ka oo + bay ka dhigysaa + hadii uu yaay nah -ka dhigsaa
+
+maximum = Math.max(x,y,z);
+minumum = Math.max(x,y,z);
+
+
+console.log(maximum);
+console.log(minumum);
+
