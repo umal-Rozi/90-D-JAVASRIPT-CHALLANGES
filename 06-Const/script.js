94c46e619011424a78663933e7e6d3771b14d494
umalrozi <umalrozi@email.com>
2023-01-25 14:11:51 -0800

    commit

---
+++ 06-Const/script.js
@@ -0,0 +1,32 @@
+
+// const is variable that con't be changed 
+
+let pi=3.234;
+let radius;
+let circ;
+
+radius=window.prompt("enter");
+radius=Number(radius);                    ////////// this varible chagedd 
+
+pi=340.69;
+
+
+circ=2*pi*radius;
+console.log("the cir is:", circ);
+
+
+
+
+
+const po=3.234;
+let ra;
+let cir;
+
+radius=window.prompt("enter");
+radius=Number(ra);                    ////////// this varible can;t changed 
+
+po=340.69;
+
+
+cir=2*po*ra;
+console.log("the cir is:", cir);
