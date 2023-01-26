2b89f99dbd02a572262bb0cf4588a5c04854518f
umalrozi <umalrozi@email.com>
2023-01-26 13:39:47 -0800

    commit

---
+++ 12-string-slicing/main.js
@@ -0,0 +1,17 @@
+
+// slice extracts a section of a string 
+//    and return it as a new string 
+// without modifiying the origal string 
+
+
+// slice waxa uu qabtaa in uu ku so xul qayb
+
+let fullname=" umal rozi";
+let firstname;
+let lastname;
+
+firstname=fullname.slice(3);
+lastname= fullname.slice(4);
+
+console.log(firstname);
+console.log(lastname);
