eb46165a2650571fd3e00ab9515bc45296bc1016
umalrozi <umalrozi@email.com>
2023-01-26 13:21:42 -0800

    commit

---
+++ 11-useful-string-method/srript.js
@@ -0,0 +1,16 @@
+// useful string properties $ methods
+
+// length buu ku sheegyaa
+let username=" Umal rozi ";
+let phone= "123-456-789";
+console.log(username.length);
+console.log(username.charAt(0)); // index baa loogu yedhaa xarafka buu koba u yedhayaa
+console.log(username.indexOf("o")); // inta xaraf ee ku jira ayuu shegyaa
+//console.log(username.lastIndexOf(u));
+
+console.log(username);
+username= username.toUpperCase;
+username=username.toLowerCase;
+
+phone= phone.replaceAll("-","");
+console.log(phone);
