d72c1590b4884284f09ab1b229c299b3ff5e50c4
umalrozi <umalrozi@email.com>
2023-01-31 12:49:24 -0800

    commit

--- 40-array.map()/main.js
+++ 40-array.map()/main.js
@@ -2,3 +2,20 @@
 //array.map() excute a provide callback function
 // once for each array elemtn and array create new array
 
+let numbers= [1,2,3,4,5];
+let squares =numbers.map(square);
+let cubes = numbers.map(cubes);
+
+cubes.forEach(print);
+
+function square(element) {
+    return Math.pow(element, 2);
+}
+
+function cube(element) {
+    return Math.pow(element, 3);
+}
+
+function print(element) {
+    console.log(element);
+}
